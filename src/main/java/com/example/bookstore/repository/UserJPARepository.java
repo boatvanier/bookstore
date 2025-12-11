3ac0e0b07d7fbfea918643016ebf8b35fbc40658
boatconcordia <71839614+boatconcordia@users.noreply.github.com>
2025-12-11 14:30:39 -0500

    JPQL and native query

--- src/main/java/com/example/bookstore/repository/UserJPARepository.java
+++ src/main/java/com/example/bookstore/repository/UserJPARepository.java
@@ -2,13 +2,16 @@ package com.example.bookstore.repository;
 
 import com.example.bookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserJPARepository extends JpaRepository<User, Long> {
-    //Lab 2
+    //Lab 2 - JPA Query Method
     //1. Find a user by exact username
     Optional<User> findByUserName(String username);
     //2. Check if an email already exists
@@ -21,4 +24,23 @@ public interface UserJPARepository extends JpaRepository<User, Long> {
     List<User> findAllByOrderByCreatedAtDesc();
     //6. Find users created after a given date
     List<User> findByCreatedAtAfter(Timestamp date);
+
+
+    //JPQL - Java Persistence Query Language
+    @Modifying
+    @Query("delete from User u where u.email = :email")
+    void deleteByEmail(@Param("email") String email);
+
+    //Native Query
+    @Modifying
+    @Query(value = "delete from users u where u.email = :email", nativeQuery = true)
+    void deleteByEmailNative(@Param("email") String email);
+
+    //JPA Query method
+    User findByUserNameContainingAndEmailContainingIgnoreCase(String username, String email);
+
+    //JPQL
+    @Query("Select u from User u where Lower(u.userName) like Lower(concat('%', :keyword, '%'))" +
+            "or Lower(u.email) like Lower(concat('%', :keyword, '%'))")
+    User findByKeywords(@Param("keyword") String keyword);
 }
